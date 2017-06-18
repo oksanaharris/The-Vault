12859dc1135e02930b61bd8444f419108039a5af
Oksana Harr <oksana.harris@gmail.com>
2017-06-17 15:26:00 -1000

    working

--- vault.js
+++ vault.js
@@ -1,4 +1,27 @@
 'use strict';
-module.exports = function() {
+function VaultGenerator() {
+
+  const vault = {};
+  // console.log("vault: " + vault);
+
+  function getValue(key){
+    if (vault[key]){
+      return vault[key];
+    } else {
+      return null;
+    }
+  }
+
+  function setValue(key, value){
+    vault[key] = value;
+  }
   
-};
+  return {
+    getValue: getValue,
+    setValue: setValue
+  };
+}
+
+module.exports = VaultGenerator;
+
+
